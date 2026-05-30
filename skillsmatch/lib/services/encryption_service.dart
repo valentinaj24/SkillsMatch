1bf6a2ddf0f8238049c5b1e7bd9048c364d729b3
valentinaj24 <valentina.jovanovic@student.um.si>
2026-05-30 23:25:33 +0200

    Fix

--- skillsmatch/lib/services/encryption_service.dart
+++ skillsmatch/lib/services/encryption_service.dart
@@ -1,25 +1,23 @@
-import 'dart:convert';
 import 'package:encrypt/encrypt.dart';
 
 class EncryptionService {
-  // 32 karaktera = AES-256
   static final _key = Key.fromUtf8('12345678901234567890123456789012');
+  static final _iv = IV.fromUtf8('1234567890123456');
 
-  static final _iv = IV.fromLength(16);
-
-  static final _encrypter = Encrypter(AES(_key));
+  static final _encrypter = Encrypter(
+    AES(_key, mode: AESMode.cbc),
+  );
 
   static String encryptMessage(String text) {
-    final encrypted = _encrypter.encrypt(text, iv: _iv);
-    return encrypted.base64;
+    return _encrypter.encrypt(text, iv: _iv).base64;
   }
 
   static String decryptMessage(String encryptedText) {
     try {
-      final decrypted = _encrypter.decrypt64(encryptedText, iv: _iv);
-      return decrypted;
+      return _encrypter.decrypt64(encryptedText, iv: _iv);
     } catch (e) {
-      return '⚠️ Error decrypting';
+      print('DECRYPT ERROR: $e');
+      return encryptedText;
     }
   }
 }
